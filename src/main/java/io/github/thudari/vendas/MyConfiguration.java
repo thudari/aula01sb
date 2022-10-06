e76da4f1329c6eac353e071537285b34f9777a7a
thudari <thudariabib@gmail.com>
2022-10-06 10:19:56 -0300

    Criando @ customizadas

--- src/main/java/io/github/thudari/vendas/MyConfiguration.java
+++ src/main/java/io/github/thudari/vendas/MyConfiguration.java
@@ -2,11 +2,8 @@ package io.github.thudari.vendas;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("dev")
+@Dev
 public class MyConfiguration {
 	
 	@Bean
--- src/main/java/io/github/thudari/vendas/VendasApplication.java
+++ src/main/java/io/github/thudari/vendas/VendasApplication.java
@@ -1,8 +1,10 @@
 package io.github.thudari.vendas;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
-	@Value("${spring.application.name}")
+	@Value("${spring.application.name:sistema-vendas}")
 	private String appName;
+	
+	@Cachorro
+	private Animal animal;
+	
+	@Bean(name = "executarAnimal")
+	public CommandLineRunner executar() {
+		return args -> {
+			this.animal.fazerBarulho();
+		};
+	}
 	
 	@GetMapping("/hello")
 	public String helloWorld() {
